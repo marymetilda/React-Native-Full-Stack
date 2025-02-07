8c5519e540819e7ceb5c1499986736f018f2bfb3
marymetilda <metildaphy@gmail.com>
2025-02-07 17:20:11 +0530

    get all posts api

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -29,4 +29,26 @@ const createPostController = async (req, res) => {
   }
 };
 
-module.exports = { createPostController };
+// GET ALL POSTS
+const getAllPostsController = async (req, res) => {
+  try {
+    const posts = await postModel
+      .find()
+      .populate("postedBy", "_id name")
+      .sort({ createdAt: -1 });
+    return res.status(200).send({
+      success: true,
+      message: "All Posts Data",
+      posts,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Get All Posts Api",
+      error,
+    });
+  }
+};
+
+module.exports = { createPostController, getAllPostsController };
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const { requireSignIn } = require("../controllers/userController");
-const { createPostController } = require("../controllers/postController");
+const {
+  createPostController,
+  getAllPostsController,
+} = require("../controllers/postController");
 
 // router object
 const router = express.Router();
 
 // CREATE POST || POST
 router.post("/create-post", requireSignIn, createPostController);
+
+// GET ALL POSTs || GET
+router.get("/get-all-posts", getAllPostsController);
 
 // export
 module.exports = router;
