fb6669c041b6b23e419def5eaa6cac27b0d57bdf
marymetilda <metildaphy@gmail.com>
2025-02-10 10:25:43 +0530

    add delete functionality

--- client/components/PostCard.js
+++ client/components/PostCard.js
@@ -1,16 +1,64 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
 import moment from "moment";
+import React, { useState } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
-const PostCard = ({ posts }) => {
+const PostCard = ({ posts, myPostScreen }) => {
+  const [loading, setLoading] = useState(false);
+  const navigation = useNavigation();
+
+  // handle delete promt
+  const handleDeletePrompt = (id) => {
+    Alert.alert("Attention", "Are you sure you want to delete this post?", [
+      {
+        text: "Cancel",
+        onPress: () => {
+          console.log("Delete Cancelled");
+        },
+      },
+      {
+        text: "Delete",
+        onPress: handleDeletePost(id),
+      },
+    ]);
+  };
+
+  // delete post data
+  const handleDeletePost = async (id) => {
+    try {
+      setLoading(true);
+      const { data } = await axios.delete(`post/delete-post/${id}`);
+      setLoading(false);
+      alert(data?.message);
+      navigation.navigate("Home");
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+      alert(error);
+    }
+  };
+
   return (
     <View>
       <Text style={styles.heading}>Total Posts {posts?.length}</Text>
       {posts?.map((post) => (
         <View style={styles.card} key={post?._id}>
-          <Text style={styles.title}>Title: {post?.title}</Text>
+          {myPostScreen && (
+            <View>
+              <Text style={{ textAlign: "right" }}>
+                <FontAwesome5
+                  name="trash"
+                  size={16}
+                  color="red"
+                  onPress={() => handleDeletePrompt(post?._id)}
+                />
+              </Text>
+            </View>
+          )}
 
+          <Text style={styles.title}>Title: {post?.title}</Text>
           <Text style={styles.desc}>{post?.description}</Text>
           <View style={styles.footer}>
             {post?.postedBy?.name && (
